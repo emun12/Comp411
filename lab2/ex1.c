4a20788bdbddbdd8110a4685f9f15ec10336649a
emun12 <49575870+emun12@users.noreply.github.com>
2021-07-12 09:22:09 -0400

    Create ex1.c

---
+++ lab2/ex1.c
@@ -0,0 +1,304 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  int a;
+
+  printf("Enter a number from 1 to 20:\n");
+  scanf("%d", &a);
+
+
+   if(a==1) {
+    printf("Here are the first %d ordinal numbers:\n",a );
+    printf("1st\n");
+  
+   }
+   else if (a==2) {
+    printf("Here are the first %d ordinal numbers:\n",a);
+    printf("1st\n");
+    printf("2nd\n"); 
+   }
+   
+   else if (a==3) {
+   printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   }
+   else if (a==4) {
+   printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+
+ }
+   else if (a==5) {
+   printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+
+  }
+  else if (a==6) {
+   printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n"); 
+  }
+  else if (a==7) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+  }
+ else if (a==8) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+  printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+}
+
+ else if (a==9) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+  printf("1st\n");  
+ printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+}
+
+ else if (a==10) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+}
+ else if (a==11) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+}
+ else if (a==12) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+  printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+}
+ else if (a==13) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+}
+
+ else if (a==14) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+  printf("14th\n");
+}
+
+ else if (a==15) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+  printf("14th\n");
+  printf("15th\n");
+}
+
+ else if (a==16) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+  printf("14th\n");
+  printf("15th\n");
+  printf("16th\n");
+}
+
+else if (a==17) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+  printf("14th\n");
+  printf("15th\n");
+  printf("16th\n");
+  printf("17th\n");
+}
+else if (a==18) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+  printf("14th\n");
+  printf("15th\n");
+  printf("16th\n");
+  printf("17th\n");
+  printf("18th\n");
+}
+
+else if (a==19) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+  printf("14th\n");
+  printf("15th\n");
+  printf("16th\n");
+  printf("17th\n");
+  printf("18th\n");
+  printf("19th\n");
+}
+
+else if (a==20) {
+  printf("Here are the first %d ordinal numbers:\n",a);
+   printf("1st\n");
+   printf("2nd\n");
+   printf("3rd\n");
+   printf("4th\n");
+   printf("5th\n");
+   printf("6th\n");
+   printf("7th\n");
+   printf("8th\n");
+   printf("9th\n");
+   printf("10th\n");
+   printf("11th\n");
+   printf("12th\n");
+  printf("13th\n");
+  printf("14th\n");
+  printf("15th\n");
+  printf("16th\n");
+  printf("17th\n");
+  printf("18th\n");
+  printf("19th\n");
+  printf("20th\n");
+}
+
+else if(a > 20) {
+printf("Number is not in the range from 1 to 20\n");
+}
+else if (a < 1) {
+printf("Number is not in the range from 1 to 20\n");
+}
+
+}
